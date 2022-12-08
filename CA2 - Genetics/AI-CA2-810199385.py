c94201f155d716cd95314dc52888dcc945cea30a
Pasha Barahimi <pashabarahimi@gmail.com>
2022-12-08 16:33:56 +0330

    CA2 working for now=)

--- CA2 - Genetics/AI-CA2-810199385.py
+++ CA2 - Genetics/AI-CA2-810199385.py
@@ -1,10 +1,25 @@
 import random
 from dataclasses import dataclass
+from timeit import default_timer as timer
 
-# TODO assign numbers to variables below
-CROSSOVER_PROBABILITY =  # probabality
-CARRY_PERCENTAGE =  # prob
-POPULATION_SIZE =  # size
+CROSSOVER_PROBABILITY = 0.7
+MUTATION_PROBABILITY = 0.05
+CARRY_PERCENTAGE = 0.2
+POPULATION_SIZE = 100
+
+OPERATOR_PRECEDENCE = {
+    '+': 1,
+    '-': 1,
+    '%': 2,
+    '*': 2
+}
+
+OPERATOR_FUNCTION = {
+    '+': lambda x, y: x + y,
+    '-': lambda x, y: x - y,
+    '%': lambda x, y: x % y,
+    '*': lambda x, y: x * y
+}
 
 
 @dataclass
@@ -15,82 +30,111 @@ class EquationBuilder:
     goalNumber: int
     population: list[list]
 
-    def __init__(self, operators, operands, equationLength, goalNumber):
+    def __init__(self, operators: list[str], operands: list[int], equationLength: int, goalNumber: int):
         self.operators = operators
         self.operands = operands
         self.equationLength = equationLength
         self.goalNumber = goalNumber
 
-        # Create the earliest population at the begining
         self.population = self.makeFirstPopulation()
 
-    def makeFirstPopulation(self):
-        # TODO create random chromosomes to build the early population, and return it
-        pass
+    def makeFirstPopulation(self) -> list[list]:
+        operandCount = int(self.equationLength / 2) + 1
+        operatorCount = int(self.equationLength / 2)
 
-    def findEquation(self):
-        # Create a new generation of chromosomes, and make it better in every iteration
+        population = []
+        for _ in range(POPULATION_SIZE):
+            chromosome = [0] * self.equationLength
+            for j in range(operandCount):
+                chromosome[j * 2] = random.choice(self.operands)
+            for j in range(operatorCount):
+                chromosome[j * 2 + 1] = random.choice(self.operators) # type: ignore
+            population.append(chromosome)
+        return population
+
+    def findEquation(self) -> list:
         while (True):
             random.shuffle(self.population)
 
-            fitnesses = []
-            for i in range(POPULATION_SIZE):
-                # TODO calculate the fitness of each chromosome
-                # TODO return chromosome if a solution is found, else save the fitness in an array
-                pass
+            fitnesses = [self.calcFitness(self.population[i]) for i in range(POPULATION_SIZE)]
+            if min(fitnesses) == 0:
+                return self.population[fitnesses.index(0)]
 
-            # TODO find the best chromosomes based on their fitnesses, and carry them directly to the next generation (optional)
+            bestChromosomes = [x for _, x in sorted(zip(fitnesses, self.population), key=lambda pair: pair[0])]
             carriedChromosomes = []
             for i in range(0, int(POPULATION_SIZE * CARRY_PERCENTAGE)):
-                carriedChromosomes.append(bestChromosome[i])
+                carriedChromosomes.append(bestChromosomes[i])
 
-            # A pool consisting of potential candidates for mating (crossover and mutation)
-            matingPool = self.createMatingPool()
-
-            # The pool consisting of chromosomes after crossover
+            matingPool = self.createMatingPool(bestChromosomes)
             crossoverPool = self.createCrossoverPool(matingPool)
-
-            # Delete the previous population
             self.population.clear()
 
-            # Create the portion of population that is undergone crossover and mutation
-            for i in range(POPULATION_SIZE - int(POPULATION_SIZE*CARRY_PERCENTAGE)):
+            for i in range(POPULATION_SIZE - int(POPULATION_SIZE * CARRY_PERCENTAGE)):
                 self.population.append(self.mutate(crossoverPool[i]))
 
-            # Add the prominent chromosomes directly to next generation
             self.population.extend(carriedChromosomes)
 
-    def createMatingPool(self):
-        # TODO make a brand new custom pool to accentuate prominent chromosomes (optional)
-        # TODO create the matingPool using custom pool created in the last step and return it
-        return matingPool
+    def createMatingPool(self, bestChromosomes: list[list]) -> list[list]:
+        ranks = list(reversed(range(1, POPULATION_SIZE + 1)))
+        matingPool = []
+        for i in range(POPULATION_SIZE):
+            for _ in range(ranks[i]):
+                matingPool.append(bestChromosomes[i])
+        random.shuffle(matingPool)
+        return matingPool[:POPULATION_SIZE]
 
-    def createCrossoverPool(self, matingPool):
+    def createCrossoverPool(self, matingPool: list[list]) -> list[list]:
         crossoverPool = []
-        for i in range(len(matingPool)):
+        for i in range(0, len(matingPool) - 1, 2):
             if random.random() > CROSSOVER_PROBABILITY:
-                # TODO don't perform crossover and add the chromosomes to the next generation directly to crossoverPool
-                pass
+                crossoverPool.append(matingPool[i])
+                crossoverPool.append(matingPool[i + 1])
             else:
-                # TODO find 2 child chromosomes, crossover, and add the result to crossoverPool
-                pass
+                children = self.crossover(matingPool[i], matingPool[i + 1])
+                crossoverPool.extend(children)
         return crossoverPool
 
-    def mutate(self, chromosome):
-        # TODO mutate the input chromosome
+    def crossover(self, chromosome1: list, chromosome2: list) -> tuple[list, list]:
+        crossoverPoint1 = random.randint(0, self.equationLength - 1)
+        crossoverPoint2 = random.randint(0, self.equationLength - 1)
+        if crossoverPoint1 > crossoverPoint2:
+            crossoverPoint1, crossoverPoint2 = crossoverPoint2, crossoverPoint1
+
+        chromosome1 = list(chromosome1)
+        chromosome2 = list(chromosome2)
+        for i in range(crossoverPoint1, crossoverPoint2):
+            chromosome1[i], chromosome2[i] = chromosome2[i], chromosome1[i]
+
+        return chromosome1, chromosome2
+
+    def mutate(self, chromosome: list) -> list:
+        chromosome = list(chromosome)
+        for i in range(len(chromosome)):
+            if random.random() < MUTATION_PROBABILITY:
+                if chromosome[i] in self.operators:
+                    chromosome[i] = random.choice(self.operators)
+                else:
+                    chromosome[i] = random.choice(self.operands)
         return chromosome
 
-    def calcFitness(self, chromosome):
-        # TODO define the fitness measure here
-        pass
+    def calcFitness(self, chromosome: list) -> int:
+        return abs(self.goalNumber - eval("".join(map(str, chromosome))))
 
 
-operands = [1, 2, 3, 4, 5, 6, 7, 8]
-operators = ['+', '-', '*']
-equationLength = 21
-goalNumber = 18019
+def main():
+    operands = [1, 2, 3, 4, 5, 6, 7, 8]
+    operators = ['+', '-', '*']
+    equationLength = 21
+    goalNumber = 18019
 
-equationBuilder = EquationBuilder(
-    operators, operands, equationLength, goalNumber)
-equation = equationBuilder.findEquation()
-print(equation)
+    equationBuilder = EquationBuilder(
+        operators, operands, equationLength, goalNumber)
+    start = timer()
+    equation = equationBuilder.findEquation()
+    end = timer()
+    print(*equation, "=", eval("".join(map(str, equation))))
+    print(f"Build Time: {(end - start):.4f}s")
+
+
+if __name__ == "__main__":
+    main()
