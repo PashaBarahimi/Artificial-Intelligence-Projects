084b06b22871f55c393c1ba272ae7a502448684f
Pasha Barahimi <pashabarahimi@gmail.com>
2022-12-13 16:23:41 +0330

    Refactor CA3 code

--- CA3 - Game/AI-CA3-810199385.py
+++ CA3 - Game/AI-CA3-810199385.py
@@ -1,4 +1,3 @@
-from select import select
 import turtle
 import math
 import random
@@ -7,6 +6,11 @@ from enum import Enum
 from dataclasses import dataclass
 from typing import Union
 from sys import argv
+
+VERTICES_COUNT = 6
+SLEEP_TIME = 0.5
+
+Line = tuple[int, int]
 
 
 class Player(Enum):
@@ -17,39 +21,26 @@ class Player(Enum):
         return Player.RED if self == Player.BLUE else Player.BLUE
 
 
-@dataclass
-class Sim:
-    _gui: bool
-    _screen: Union[turtle._Screen, None]
-    _selection: list
-    _turn: Player
-    _dots: list
-    _player_moves: dict
-    _available_moves: list
-    _minimax_depth: int
-    _prune: bool
+class SimGui:
+    Dot = tuple[float, float]
 
-    def __init__(self, minimax_depth, prune, gui):
-        self._gui = gui
-        self._prune = prune
-        self._minimax_depth = minimax_depth
-        if self._gui:
-            self._setup_screen()
-        else:
-            self._screen = None
-        self._player_moves = {
-            Player.RED: [],
-            Player.BLUE: []
-        }
-        self._initialize()
-
-    def _setup_screen(self) -> None:
+    def __init__(self, title: str, width: int, height: int, vertices_count: int):
         self._screen = turtle.Screen()
-        self._screen.setup(800, 800)
-        self._screen.title("Game of SIM")
+        self._screen.setup(width, height)
+        self._screen.title(title)
         self._screen.setworldcoordinates(-1.5, -1.5, 1.5, 1.5)
         self._screen.tracer(0, 0)
         turtle.hideturtle()
+        self._vertices_count = vertices_count
+        self._gen_dots()
+        self._draw_board()
+
+
+    def _gen_dots(self) -> None:
+        self._dots = []
+        for angle in range(0, 360, 360 // self._vertices_count):
+            self._dots.append((math.cos(math.radians(angle)),
+                               math.sin(math.radians(angle))))
 
     def _draw_dot(self, x: float, y: float, color: str) -> None:
         turtle.up()
@@ -57,27 +48,7 @@ class Sim:
         turtle.color(color)
         turtle.dot(15)
 
-    def _gen_dots(self) -> list[tuple[float, float]]:
-        r = []
-        for angle in range(0, 360, 60):
-            r.append((math.cos(math.radians(angle)),
-                     math.sin(math.radians(angle))))
-        return r
-
-    def _initialize(self) -> None:
-        self._selection = []
-        self._available_moves = []
-        for i in range(0, 6):
-            for j in range(i, 6):
-                if i != j:
-                    self._available_moves.append((i, j))
-        self._turn = Player.RED if random.randint(0, 2) == 1 else Player.BLUE
-        self._dots = self._gen_dots()
-        if self._gui:
-            turtle.clear()
-        self._draw()
-
-    def _draw_line(self, p1: tuple[float, float], p2: tuple[float, float], color: str) -> None:
+    def _draw_line(self, p1: Dot, p2: Dot, color: str) -> None:
         turtle.up()
         turtle.pensize(3)
         turtle.goto(p1)
@@ -86,24 +57,46 @@ class Sim:
         turtle.goto(p2)
 
     def _draw_board(self) -> None:
-        for i in range(len(self._dots)):
-            if i in self._selection:
-                self._draw_dot(self._dots[i][0], self._dots[i][1], self._turn.value)
-            else:
-                self._draw_dot(self._dots[i][0], self._dots[i][1], 'dark gray')
+        for dot in self._dots:
+            self._draw_dot(dot[0], dot[1], 'dark gray')
 
-    def _draw(self) -> None:
-        if not self._gui:
-            return
+    def draw(self, player_moves: dict[Player, list[Line]] = {}) -> None:
+        turtle.clear()
         self._draw_board()
-        for color, moves in self._player_moves.items():
+        for color, moves in player_moves.items():
             for move in moves:
-                self._draw_line((math.cos(math.radians(move[0] * 60)), math.sin(math.radians(move[0] * 60))),
-                                (math.cos(math.radians(move[1] * 60)), math.sin(math.radians(move[1] * 60))),
+                self._draw_line((math.cos(math.radians(move[0] * 360 // self._vertices_count)),
+                                 math.sin(math.radians(move[0] * 360 // self._vertices_count))),
+                                (math.cos(math.radians(move[1] * 360 // self._vertices_count)),
+                                 math.sin(math.radians(move[1] * 360 // self._vertices_count))),
                                 color.value)
-        if self._screen is not None:
-            self._screen.update()
-        sleep(1)
+        self._screen.update()
+
+
+@dataclass
+class Sim:
+    _gui: Union[SimGui, None]
+    _turn: Player
+    _player_moves: dict[Player, list[Line]]
+    _available_moves: list[Line]
+    _minimax_depth: int
+    _prune: bool
+
+    def __init__(self, minimax_depth: int, prune: bool, gui: bool):
+        self._prune = prune
+        self._minimax_depth = minimax_depth
+        self._gui = SimGui('Game of Sim', 800, 800, VERTICES_COUNT) if gui else None
+        self._player_moves = {player: [] for player in Player}
+
+    def _initialize(self) -> None:
+        self._available_moves = []
+        for i in range(0, VERTICES_COUNT):
+            for j in range(i, VERTICES_COUNT):
+                if i != j:
+                    self._available_moves.append((i, j))
+        self._turn = random.choice([Player.RED, Player.BLUE])
+        if self._gui:
+            self._gui.draw()
 
     def _evaluate(self):
         # TODO
@@ -116,7 +109,7 @@ class Sim:
         # TODO
         pass
 
-    def _enemy(self) -> tuple[int, int]:
+    def _enemy_move(self) -> Line:
         return random.choice(self._available_moves)
 
     def _gameover(self) -> Union[Player, None]:
@@ -131,22 +124,21 @@ class Sim:
         return None
 
     def play(self) -> Player:
+        self._initialize()
         while True:
-            if self._turn == Player.RED:
-                selection = self._minimax()[0]
-                if selection[1] < selection[0]:
-                    selection = (selection[1], selection[0])
-            else:
-                selection = self._enemy()
-                if selection[1] < selection[0]:
-                    selection = (selection[1], selection[0])
-            if selection in self._player_moves[Player.RED] or selection in self._player_moves[Player.BLUE]:
-                raise Exception("Duplicate Move!!!")
-            self._player_moves[self._turn].append(selection)
+            selection = self._minimax()[0] if self._turn == Player.RED else self._enemy_move()
+            if selection[1] < selection[0]:
+                selection = (selection[1], selection[0])
 
+            if selection in self._player_moves.values():
+                raise Exception("Duplicate Move!")
+
+            self._player_moves[self._turn].append(selection)
             self._available_moves.remove(selection)
-            selection = []
-            self._draw()
+            self._swap_turn()
+            if self._gui:
+                self._gui.draw(self._player_moves)
+                sleep(SLEEP_TIME)
             res = self._gameover()
             if res:
                 return res
@@ -155,12 +147,9 @@ class Sim:
 def main():
     game = Sim(minimax_depth=int(argv[1]), prune=True, gui=bool(int(argv[2])))
 
-    results = {
-        Player.RED: 0,
-        Player.BLUE: 0
-    }
+    results = {p: 0 for p in Player}
     for i in range(10):
-        print(f"Game {i}")
+        print(f"Game {i}:")
         results[game.play()] += 1
 
     print(results)
